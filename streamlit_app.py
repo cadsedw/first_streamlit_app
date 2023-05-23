9ada2136cd1c88d2e641b3d4b0ccdeef094e1020
cadsedw <133966206+cadsedw@users.noreply.github.com>
2023-05-23 11:11:08 +0100

    Update streamlit_app.py
    imported pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.header('cads streamlit')
 streamlit.text('writing some text')
 streamlit.text('🥣 🥗 🐔 🥑🍞')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
